c0b95cac8eb2928d0b5d6d65fee938fe97f60262
zfjsail <zfjsail@163.com>
2020-06-15 22:14:33 +0800

    fix

--- gae/utils.py
+++ gae/utils.py
@@ -182,7 +182,7 @@ def get_roc_score(emb, adj_orig, edges_pos, edges_neg):
         neg.append(adj_orig[e[0], e[1]])
 
     preds_all = np.hstack([preds, preds_neg])
-    labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds))])
+    labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds_neg))])
     roc_score = roc_auc_score(labels_all, preds_all)
     ap_score = average_precision_score(labels_all, preds_all)
 
